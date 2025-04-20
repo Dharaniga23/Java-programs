17b4c2f6eba692790f90eab873c07090f193a079
Dharaniga23 <dharaniga23@gmail.com>
2025-04-20 12:39:28 +0530

    adding new package

---
+++ examples/swap.java
@@ -0,0 +1,17 @@
+package examples;
+
+public class swap {
+
+    public static void main(String[]args){
+        /*System.out.println("\"Hello World\"");
+        System.out.println("my name is dharaniga");*/
+        String m="narthu";
+        String n="dharani";
+        String temp;
+        temp=m;
+        m=n;
+        n=temp;
+        System.out.println("my name is:"+m);
+        System.out.println("my name is:"+n);
+    }
+}
